aae8966606c7f4d96f9f330e1fc416ee9512f20e
ViiSall <visal8779@gmail.com>
2024-12-26 14:23:39 +0700

    Session 3 done.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app_instance" {
   ami           = "ami-0995922d49dc9a17d"
   instance_type = "t2.micro"
-  key_name      = "final_cloud_project"
+  key_name      = "cloud_project"
 
   tags = {
     Name = "TodoAppInstance"
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -9,6 +9,6 @@ resource "aws_db_instance" "mysql_db" {
   parameter_group_name = "default.mysql8.0"
 
   tags = {
-    Name = "TodoAppDatabase"
+    Name = "TodoAppDB"
   }
 }
