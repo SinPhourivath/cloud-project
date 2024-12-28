df2dd5f0ddffce84455777e458c4ea0c2afe3a0d
Hout <Houtkok123@gmail.com>
2024-12-28 22:28:52 +0700

    terraform make change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,32 @@
+provider "aws"{
+  profile = "all_profile"
+}
 resource "aws_instance" "app_instance" {
   ami           = "ami-0995922d49dc9a17d"
   instance_type = "t2.micro"
-  key_name      = "final_cloud_project"
+  key_name      = "cloud_project"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  user_data = <<-EOF
+              #install all package
+              sudo yum update -y
+
+              #install docker
+              yum install -y docker
+              sudo service docker start
+              chkconfig docker on
+              sudo usermod -aG docker ec2-user
+
+              #aws cli
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              sudo ./aws/install
+
+              #install mysql client
+              sudo yum update -y
+              sudo yum install -y mysql
+
+              EOF
 
   tags = {
     Name = "TodoAppInstance"
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,12 +1,25 @@
+data "aws_kms_secrets" "creds"{
+    secret {
+        name = "db"
+        payload = file("${path.module}/db-creds.yml.encrypted")
+    }
+}
+locals {
+  db_creds = yamldecode(data.aws_kms_secrets.creds.plaintext["db"])
+}
+
 resource "aws_db_instance" "mysql_db" {
   allocated_storage    = 20
   engine               = "mysql"
   engine_version       = "8.0.35"
   instance_class       = "db.t3.micro"
   db_name              = "todo_app_db"
-  username             = "admin"
-  password             = "Admin#123"
+  username             = local.db_creds.username
+  password             = local.db_creds.password
+  publicly_accessible  = true
   parameter_group_name = "default.mysql8.0"
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot  = true
 
   tags = {
     Name = "TodoAppDatabase"
